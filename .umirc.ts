5576b8733aa6bdf2118dea27180ae9ce29e003fd
CentChuan <609833577@qq.com>
2021-05-19 17:56:25 +0800

    feat: 修改配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,17 @@
-import {defineConfig} from 'dumi'
+import { defineConfig } from 'dumi';
+
+let BaseUrl = '/cent-react-wheels';
 
 export default defineConfig({
   title: 'cent-react-wheel',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
-
+  base: BaseUrl,
+  publicPath: BaseUrl + '/', // 打包文件时，引入地址生成 publicPath/xxx.js
+  exportStatic: {}, // 对每隔路由输出html
+  dynamicImport: {}, // 动态导入
 });
